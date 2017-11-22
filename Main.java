cb0745fcaa4cfdad8b47948e5f420224cc0622ad
smiroshnikov <sergeim.qa@gmail.com>
2017-11-23 01:00:47 +0200

    recording only GET when looking for porn

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 package proxy;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import net.lightbody.bmp.BrowserMobProxy;
@@ -49,6 +51,10 @@ public class Main {
         driver.get("http://www.google.com/xhtml");
         driver.manage().window().maximize();
         Thread.sleep(15000);
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("PORN FREE!");
+        searchBox.submit();
+        Thread.sleep(5000);
     }
 
 
@@ -58,7 +64,10 @@ public class Main {
 
             // Get the HAR data
             Har har = server.getHar();
-            File harFile = new File("C:\\Webdrivers\\google_test.har");
+
+
+            File harFile = new File("C:\\Webdrivers\\google_test.json");
+            har.getLog().getEntries().removeIf(x-> !x.getRequest().getMethod().equals("GET"));
             har.writeTo(harFile);
 
         } catch (IOException ioe) {
