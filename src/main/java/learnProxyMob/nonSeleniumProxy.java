15825a415ffc5084d1d505fac35c5d5e51287d45
smiroshn <smiroshn@akamai.com>
2017-11-30 13:47:07 +0200

    binding browsermob with selenium proxy - working

--- src/main/java/learnProxyMob/nonSeleniumProxy.java
+++ src/main/java/learnProxyMob/nonSeleniumProxy.java
@@ -2,6 +2,7 @@ package learnProxyMob;
 
 import net.lightbody.bmp.BrowserMobProxy;
 import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.core.har.Har;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
@@ -15,20 +16,26 @@ public class nonSeleniumProxy {
 
     public static void main(String[] args) {
 
+
         System.setProperty("webdriver.chrome.driver", "/Users/smiroshn/work/chromedriver/chromedriver");
         // Creating browsermob proxy in order to use browsermob
         BrowserMobProxyServer browserMobProxyServer = new BrowserMobProxyServer();
+
+        Proxy seleniumProxy = new Proxy();
+
+
+
+
         browserMobProxyServer.start();
 
         // creating selenium proxy in order to bind it to browsermob ?
-        Proxy seleniumProxy = new Proxy();
+
 
         // binding chromedriver to work with selenium proxy
-        DesiredCapabilities browserCapabilities = new DesiredCapabilities();
+        DesiredCapabilities browserCapabilities = DesiredCapabilities.chrome();
         browserCapabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
 
-        WebDriver driver;
-        driver = new ChromeDriver(browserCapabilities);
+        WebDriver driver = new ChromeDriver(browserCapabilities);
 
         // what is point of creating  tag ?
         browserMobProxyServer.newHar("wpa.har");
